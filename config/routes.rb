05addba72f8d6d8c39d1d840df4828c079033114
AmandaS <mandyschmitz2107@gmail.com>
2018-05-18 19:20:10 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create]  do
     resources :doses, only: [:create, :destroy]
   end
+  root to: 'cocktails#index'
 end
 
